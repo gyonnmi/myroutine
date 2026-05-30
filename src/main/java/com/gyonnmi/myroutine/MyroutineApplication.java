0b011ed1bbb9b5897bab838e91a9593eb1acdd33
giyonmi.gu <user033.dounyu2026@tmns-3ssscom.net>
2026-05-31 00:15:54 +0900

    first commit

---
+++ src/main/java/com/gyonnmi/myroutine/MyroutineApplication.java
@@ -0,0 +1,13 @@
+package com.gyonnmi.myroutine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyroutineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyroutineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gyonnmi/myroutine/MyroutineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyonnmi.myroutine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyroutineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
