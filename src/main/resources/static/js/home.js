463396aa385cfb92b6728baf96cf69f1e57c9e40
giyonmi.gu <user033.dounyu2026@tmns-3ssscom.net>
2026-06-04 09:50:13 +0900

    feat: 開閉可能なサイドバーを実装

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -185,3 +185,22 @@ routineForm.addEventListener('submit', () => {
 });
 
 validateRepeatDays();
+
+const sidebarToggle = document.getElementById('sidebarToggle');
+const sidebarClose = document.getElementById('sidebarClose');
+const sidebarOverlay = document.getElementById('sidebarOverlay');
+const sidebar = document.getElementById('sidebar');
+
+function openSidebar() {
+  sidebar.classList.add('active');
+  sidebarOverlay.classList.add('active');
+}
+
+function closeSidebar() {
+  sidebar.classList.remove('active');
+  sidebarOverlay.classList.remove('active');
+}
+
+sidebarToggle.addEventListener('click', openSidebar);
+sidebarClose.addEventListener('click', closeSidebar);
+sidebarOverlay.addEventListener('click', closeSidebar);
